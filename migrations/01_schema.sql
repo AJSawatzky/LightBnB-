3c12324787be7a8e9833a11cdead5e907135ff77
AJSawatzky <andrassawatzky@gmail.com>
2023-01-30 23:42:40 -0800

    Spacing and linting errors

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -1,10 +1,7 @@
-CREATE DATABASE lightbnb;
-\c lightbnb
-
-DROP TABLE IF EXISTS users CASCADE;
-DROP TABLE IF EXISTS properties CASCADE;
-DROP TABLE IF EXISTS reservations CASCADE;
-DROP TABLE IF EXISTS property_reviews CASCADE;
+--DROP TABLE IF EXISTS users CASCADE;
+--DROP TABLE IF EXISTS properties CASCADE;
+--DROP TABLE IF EXISTS reservations CASCADE;
+--DROP TABLE IF EXISTS property_reviews CASCADE;
 
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
@@ -16,7 +13,6 @@ CREATE TABLE users (
 CREATE TABLE properties (
   id SERIAL PRIMARY KEY NOT NULL,
   owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-
   title VARCHAR(255) NOT NULL,
   description TEXT,
   thumbnail_photo_url VARCHAR(255) NOT NULL,
@@ -25,13 +21,11 @@ CREATE TABLE properties (
   parking_spaces INTEGER  NOT NULL DEFAULT 0,
   number_of_bathrooms INTEGER  NOT NULL DEFAULT 0,
   number_of_bedrooms INTEGER  NOT NULL DEFAULT 0,
-
   country VARCHAR(255) NOT NULL,
   street VARCHAR(255) NOT NULL,
   city VARCHAR(255) NOT NULL,
   province VARCHAR(255) NOT NULL,
   post_code VARCHAR(255) NOT NULL,
-
   active BOOLEAN NOT NULL DEFAULT TRUE
 );
 
